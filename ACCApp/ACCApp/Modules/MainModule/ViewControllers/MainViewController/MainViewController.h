085fe0f9439de4df199e627020bbacc1c72ef320
Bin Yu <binyu.fi@gmail.com>
2017-03-25 13:13:25 +0200

    Added main module.

---
+++ ACCApp/ACCApp/Modules/MainModule/ViewControllers/MainViewController/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ACCApp
+//
+//  Created by Bin Yu on 25/03/2017.
+//  Copyright © 2017 Bin Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UINavigationController
+
+@end
